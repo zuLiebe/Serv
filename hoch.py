b543b3f578e8bf61c2b1303f497c4f2d030813fe
zuLiebe <139193908+zuLiebe@users.noreply.github.com>
2023-07-14 23:49:24 +0200

    Update hoch.py

--- hoch.py
+++ hoch.py
@@ -3,11 +3,13 @@ import time
 
 scripts = ['altona.py', 'fluwog.py', 'mein_scraper.py', 'mein_sender.py']
 processes = []
+stop_count = 0
 
 # Запуск всех скриптов
 for script in scripts:
     process = subprocess.Popen(['python', script])
     processes.append(process)
+    print(f'Started script: {script}')
 
 # Проверка состояния скриптов каждые 25 секунд
 while True:
@@ -16,5 +18,10 @@ while True:
             # Скрипт завершился, завершаем все остальные процессы
             for p in processes:
                 p.terminate()
-            break
+            stop_count += 1
+            print(f'Script {process.args} stopped')
+
+    if stop_count == 0:
+        print('All scripts running fine')
+
     time.sleep(25)
