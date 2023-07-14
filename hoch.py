8fbe67ab3a09aa3da64b1428c19e6d02897a7a1d
zuLiebe <139193908+zuLiebe@users.noreply.github.com>
2023-07-14 23:23:28 +0200

    Create hoch.py

---
+++ hoch.py
@@ -0,0 +1,20 @@
+import subprocess
+import time
+
+scripts = ['fluwog.py', 'mein_scraper.py', 'mein_sender.py']
+processes = []
+
+# Запуск всех скриптов
+for script in scripts:
+    process = subprocess.Popen(['python', script])
+    processes.append(process)
+
+# Проверка состояния скриптов каждые 25 секунд
+while True:
+    for process in processes:
+        if process.poll() is not None:
+            # Скрипт завершился, завершаем все остальные процессы
+            for p in processes:
+                p.terminate()
+            break
+    time.sleep(25)
