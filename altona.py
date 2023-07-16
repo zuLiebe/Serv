db638ca4fa52bad3f3f2fb7a88bf24f3ebc3f6cd
zuLiebe <139193908+zuLiebe@users.noreply.github.com>
2023-07-16 10:42:11 +0200

    Update altona.py

--- altona.py
+++ altona.py
@@ -17,8 +17,8 @@ def check_results():
         result_text = result_element.text.strip()
         if result_text != 'Momentan sind leider keine Immobilien in unserem Angebot verfügbar.':
             # Отправка уведомления в Telegram
-            bot_token = '6187859381:AAGQaohroWL-bxwVVf32lLNtBF-WY61A6Js'
-            chat_id = '-913675079'
+            bot_token = '1736081100:AAF2xjTtTjK5jNBVql-rU2bZe8NYcaMm_H4'
+            chat_id = '-1001949522816'
             bot = Bot(token=bot_token)
             message = 'Объявления появились на сайте! Ссылка: {}'.format(url)
             bot.send_message(chat_id=chat_id, text=message)
